99e6f0f49c9a86a9c8f8fd16d2e2167dac6940f4
David Newhall II <david@sleepers.pro>
2024-03-16 22:23:53 -0700

    make some safety symlinks

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 # You can run this locally, you just need to gem install fpm.
 # See the release.yml workflow file for a command to do that.
 
-MAINT="David Newhall II <captain at golift dot io>"
+##
 DESC="Official Worker for Notifiarr.com"
 LICENSE="GPLv2"
-# Used for source links in package metadata.
+MAINT="David Newhall II <captain at golift dot io>"
 SOURCE_URL="https://github.com/Notifiarr/workers"
 VENDOR="Go Lift <code@golift.io>"
-
+#
 DATE="$(date -u +%Y-%m-%dT%H:%M:00Z)"
 VERSION=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1) 2>/dev/null | tr -d v)
 [ "$VERSION" != "" ] || VERSION=development
@@ -17,37 +17,39 @@ ITERATION=$(git rev-list --count --all || echo 0)
 COMMIT="$(git rev-parse --short HEAD || echo 0)"
 GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD || echo unknown)"
 BRANCH="${GIT_BRANCH:-${GITHUB_REF_NAME}}"
-
+#
 SIGNING_KEY=B93DD66EF98E54E2EAE025BA0166AD34ABC5A57C
 PACKAGE_NAME="notifiarr-worker"
+##
 
 read -r -d '' DEPENDS <<- DEPENDS
-    --depends supervisor
+    --depends mysql-client-core-8.0
     --depends nfs-client
-    --depends telegraf
-    --depends software-properties-common
-    --depends sudo
-    --depends php8.3-redis
+    --depends php8.3-cli
+    --depends php8.3-curl
+    --depends php8.3-mbstring
     --depends php8.3-memcached
     --depends php8.3-mysql
-    --depends php8.3-mbstring
-    --depends php8.3-curl
+    --depends php8.3-redis
     --depends php8.3-xml
-    --depends mysql-client-core-8.0
+    --depends software-properties-common
+    --depends sudo
+    --depends supervisor
+    --depends telegraf
 DEPENDS
 
 read -r -d '' PACKAGE_ARGS <<- PACKAGE_ARGS
-    --before-install before-install.sh
     --after-install after-install.sh
+    --before-install before-install.sh
     --before-remove before-remove.sh
-    --name ${PACKAGE_NAME}
     --deb-no-default-config-files
+    --description='${DESC}'
     --iteration ${ITERATION}
     --license ${LICENSE}
-    --url ${SOURCE_URL}
     --maintainer='${MAINT}'
+    --name ${PACKAGE_NAME}
+    --url ${SOURCE_URL}
     --vendor='${VENDOR}'
-    --description='${DESC}'
 PACKAGE_ARGS
 
 mkdir -p root/var/log/workers
