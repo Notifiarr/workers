18966425acb18c51cd2edbb717253d6f005bbfe3
David Newhall II <david@sleepers.pro>
2024-03-18 21:30:00 -0700

    missed a save2

--- root/config/supervisor/prestart.sh
+++ root/config/supervisor/prestart.sh
@@ -12,7 +12,7 @@ ln -sf "/share/websites/confs/server.$(hostname -s)" "/config/server.json"
 # We also create a symlink to a server-specific log directory in shared storage.
 ln -Tsf "/share/logs/notifiarr/supervisor/$(hostname -s)" "/config/log"
 
-# This script creates (builds) supervisor configurations for this server.
+# This php script creates (builds) the supervisord configurations for this server.
 php /share/websites/www/notifiarr.com/supervisor/confBuilder.php
 # The files created by confBuilder.php go in this directory. Fix their ownership.
 chown -R abc: "/share/workers/$(hostname -s)/supervisor"
