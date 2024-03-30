5f060c0be88d5c94f5e76a21cc9242e573397567
David Newhall II <david@sleepers.pro>
2024-03-30 12:44:53 -0700

    save ssh keys

--- after-install.sh
+++ after-install.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+# Backup our ssh keys if they aren't already.
+if [ -d "/share/workers/defaults/ssh" ] && [ ! -d "/share/workers/defaults/ssh/$(hostname -s)" ]; then
+  mkdir "/share/workers/defaults/ssh/$(hostname -s)"
+  cp -r /etc/ssh/ssh_host_* "/share/workers/defaults/ssh/$(hostname -s)"
+fi
+
 if [ -d ~abc/.ssh ]; then
   chown -R abc: ~abc
 fi
@@ -14,4 +20,12 @@ if [ -x "/bin/systemctl" ]; then
   if [ -d "/share/websites/www" ]; then
     /bin/systemctl restart supervisor
   fi
+fi
+
+# Replace the ssh keys with the backup if they're different; then restart sshd.
+if [ -d "/share/workers/defaults/ssh/$(hostname -s)" ] && \
+  ! diff -q "/share/workers/defaults/ssh/$(hostname -s)/ssh_host_ecdsa_key" /etc/ssh/ssh_host_ecdsa_key >/dev/null 2>&1
+then
+  cp -r "/share/workers/defaults/ssh/$(hostname -s)/ssh_host_*" /etc/ssh/
+  /bin/systemctl restart sshd
 fi
